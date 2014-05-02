1f2b81d535428c490fe64ee975d6f82f6d33da31
Nathan Bolser <nathan.bolser@hungrymachine.com>
2014-05-01 22:10:52 -0600

    inital commit - working example

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+ provider :facebook, '1417807295150006', '002a546fc9959a3a7222352bdfb6eea8'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OmniauthTutorial::Application.config.secret_key_base = 'aa50b41cfee19dd4eea89cb284a578af2914be1deaa9dda5f28d559986e7fa6d0538d02051dd391b10217f24598a1462685bde0f3da7e671caf7a656c8bea8cd'
